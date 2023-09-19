5eba709801f9c2bbf1cdd360341d6297e429618d
pedro <pedroajs93@gmail.com>
2023-09-19 12:16:12 -0300

    relations

--- demo/src/main/java/com/example/demo/entities/User.java
+++ demo/src/main/java/com/example/demo/entities/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_user")
@@ -18,6 +20,13 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     public User(){}
 
