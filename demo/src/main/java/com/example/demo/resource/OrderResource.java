4dba1f57ca030021d76f6a7275d5c6ca1dbb578b
pedro <pedroajs93@gmail.com>
2023-09-19 18:18:01 -0300

    order entity, repository, resouce and service

---
+++ demo/src/main/java/com/example/demo/resource/OrderResource.java
@@ -0,0 +1,31 @@
+package com.example.demo.resource;
+
+import com.example.demo.entities.Order;
+import com.example.demo.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/order")
+public class OrderResource {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll(){
+        List<Order> list = orderService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> finById(@PathVariable Long id){
+        Order result = orderService.findById(id);
+        return ResponseEntity.ok().body(result);
+    }
+}
